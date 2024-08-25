e7d08a99d35392539a7d7356c4d78cfeb87de912
Daniel O'Sullivan <daniel@osullivan.io>
2024-08-25 16:51:56 -0500

    add basic proxy endpoint

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
+	"net/http"
 	"strings"
 
 	"goes-api-go/config"
@@ -48,6 +50,31 @@ func main() {
 	router.GET("/archive/:date", handlers.GetImagesByDate(s3Client))
 	router.GET("/available-dates", handlers.GetAvailableDates(s3Client))
 
+	router.GET("/proxy/image", func(c *gin.Context) {
+		imageUrl := c.Query("url")
+		if imageUrl == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Image URL is required"})
+			return
+		}
+
+		resp, err := http.Get(imageUrl)
+		if err != nil || resp.StatusCode != http.StatusOK {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch image"})
+			return
+		}
+		defer resp.Body.Close()
+
+		// Copy the image content to the response
+		for k, v := range resp.Header {
+			c.Header(k, v[0])
+		}
+		c.Stream(func(w io.Writer) bool {
+			_, err := io.Copy(w, resp.Body)
+			return err == nil
+		})
+	})
+
+	// Run the server
 	if err := router.Run(":" + cfg.Port); err != nil {
 		log.Fatalf("Failed to run server: %v", err)
 	}
